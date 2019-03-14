be06ecd19349d683161efcd5c7b5e446ffb5f1ad
EddyZuefflingen <47632023+EddyZuefflingen@users.noreply.github.com>
2019-03-14 13:29:18 +0100

    Nur Ordentlichkeit

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -1,30 +1,33 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-Class Login_model extends CI_Model
+class Login_model extends CI_Model
 {
-  public function CheckIfUsernameExists($Username)
-  {
-    $this->load->database();
-    $query = $this->db->query("SELECT username FROM accounts WHERE username = '".$Username."'");
-    if ($query->row() == null) return false;
-    else return true;
-  }
+    public function CheckIfUsernameExists($Username)
+    {
+        $this->load->database();
+        $query = $this->db->query("SELECT username FROM accounts WHERE username = '" . $Username . "'");
+        if ($query->row() == null)
+            return false;
+        else
+            return true;
+    }
 
-  public function CheckIfEmailExists($email)
-  {
-    $this->load->database();
-    $query = $this->db->query("SELECT email FROM accounts WHERE email = '".$email."'");
-    if ($query->row() == null) return false;
-    else return true;
-  }
-  
-  public function GetAccountData($Username) 
-  {
-		$this->load->database();
-    $query = $this->db->query("SELECT * FROM accounts WHERE username = '".$Username."'");
-    $RowResult = $query->row();
-    return $RowResult;
-  }
+    public function CheckIfEmailExists($email)
+    {
+        $this->load->database();
+        $query = $this->db->query("SELECT email FROM accounts WHERE email = '" . $email . "'");
+        if ($query->row() == null)
+            return false;
+        else
+            return true;
+    }
+
+    public function GetAccountData($Username)
+    {
+        $this->load->database();
+        $query = $this->db->query("SELECT * FROM accounts WHERE username = '" . $Username . "'");
+        $RowResult = $query->row();
+        return $RowResult;
+    }
 }
-?>
--- application/views/login_view.php
+++ application/views/login_view.php
@@ -3,41 +3,41 @@ defined('BASEPATH') or exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 
-<head>
-	<meta charset="utf-8">
-	<link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>css/login_view.css">
-	</head>
-<body>
-	<div>
-		<?php
-$this->load->helper('form');
-echo form_open('Login_Controller/LoginRegisterSwitch');
-$UsernameFormSettings = array(
-    'name' => 'username',
-    'id' => 'username',
-    'maxlength' => '100',
-    'size' => '50',
-    'style' => 'width:50%',
-);
+    <head>
+        <meta charset="utf-8">
+        <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>css/login_view.css">
+    </head>
+    <body>
+        <div>
+            <?php
+                $this->load->helper('form');
+                echo form_open('Login_Controller/LoginRegisterSwitch');
+                $UsernameFormSettings = array(
+                    'name' => 'username',
+                    'id' => 'username',
+                    'maxlength' => '100',
+                    'size' => '50',
+                    'style' => 'width:50%',
+                );
 
-$PasswordFormSettings = array(
-    'name' => 'password',
-    'id' => 'password',
-    'maxlength' => '100',
-    'size' => '50',
-    'style' => 'width:50%',
-);
+                $PasswordFormSettings = array(
+                    'name' => 'password',
+                    'id' => 'password',
+                    'maxlength' => '100',
+                    'size' => '50',
+                    'style' => 'width:50%',
+                );
 
-echo "Username: " . form_input($UsernameFormSettings);
-echo "<br><br>";
-echo "Password: " . form_password($PasswordFormSettings);
-echo "<br><br>";
+                echo "Username: " . form_input($UsernameFormSettings);
+                echo "<br><br>";
+                echo "Password: " . form_password($PasswordFormSettings);
+                echo "<br><br>";
 
-echo form_submit('Login', 'Login');
-echo form_submit('Register', 'Register', 'Register');
-echo form_submit('Test', 'MarvinsButton');
-echo form_close();
-?>
-	</div>
-</body>
+                echo form_submit('Login', 'Login');
+                echo form_submit('Register', 'Register', 'Register');
+                echo form_submit('Test', 'MarvinsButton');
+                echo form_close();
+            ?>
+        </div>
+    </body>
 </html>
