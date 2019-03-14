ab36054adb49f411820e96a8fc33936fa2ae64dc
EddyZuefflingen <47632023+EddyZuefflingen@users.noreply.github.com>
2019-03-14 13:21:10 +0100

    Soos

--- application/controllers/Login_Controller.php
+++ application/controllers/Login_Controller.php
@@ -1,35 +1,58 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login_Controller extends CI_Controller {
+class LoginController extends CI_Controller
+{
 
-	public function index()
-	{
-		$this->load->view("login_view");
-	}
+    public function index()
+    {
+        $this->load->view("login_view");
+        $this->load->helper('url');
+    }
 
-	public function Login()
-	{
-		$this->load->view("login_view");
-		$this->load->helper("form");
-	}
+    public function Login()
+    {
+        $this->load->view("login_view");
+        $this->load->helper("form");
+    }
 
-	public function LoginRegisterSwitch()
-	{
-		if($this->input->post("Login") !== null) $this->LoginValidation();
-		else if ($this->input->post("Register") !== null) $this->load->view("register_view");
-		else if ($this->input->post("Test") !== null) $this->load->view("input_view"); // Workaround, wird später entfernt
-	}
+    public function LoginRegisterSwitch()
+    {
+        if ($this->input->post("Login") !== null)
+        {
+            $this->LoginValidation();
+        }
+        elseif ($this->input->post("Register") !== null)
+        {
+            $this->load->view("register_view");
+        }
+        elseif ($this->input->post("Test") !== null)
+        {
+            $this->load->view("input_view");
+        }
+        // Workaround, wird später entfernt
+    }
 
-	private function LoginValidation()
-	{
-		$this->load->model("Login_model");
-		$sqlResult = $this->Login_model->GetAccountData($this->input->post("username"));
-		if ($sqlResult !== null AND openssl_decrypt($sqlResult->password,"AES-128-CBC","UltraSavePassword",0,"0244545367373570") == $this->input->post("password"))
-		{
-			if ($sqlResult->verified == 0) exit("Account noch nicht freigeschaltet !");
-			else $this->load->view("input_view");
-		}
-		else exit("Username oder Password fehlerhaft !");
-	}
+    private function __LoginValidation()
+    {
+        $this->load->model("Login_model");
+        $sqlResult = $this->Login_model->GetAccountData($this->input->post("username"));
+        if (null !== $sqlResult and openssl_decrypt($sqlResult->password, "AES-128-CBC", "UltraSavePassword", 0, "0244545367373570") == $this->input->post("password"))
+        {
+            if (0 == $sqlResult->verified)
+            {
+                exit("Account noch nicht freigeschaltet !");
+            }
+            else
+            {
+                $this->load->view("input_view");
+            }
+
+        }
+        else
+        {
+            exit("Username oder Password fehlerhaft !");
+        }
+
+    }
 }
