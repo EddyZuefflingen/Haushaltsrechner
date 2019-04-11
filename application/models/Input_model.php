deecb94e582963ccfb1615468f4fd39b8db4ae35
Akitomaru <Stiels.marvin@web.de>
2019-04-11 14:07:57 +0200

    SomeShortFixes

--- application/models/Input_model.php
+++ application/models/Input_model.php
@@ -6,7 +6,7 @@ Class Input_model extends CI_Model
   public function loadCategories()
   {
      $this->load->database();
-     $query = $this->db->query("SELECT kategorie_ID,Kategorie FROM kategorie");
+     $query = $this->db->query("SELECT k.kategorie_ID,k.Kategorie FROM kategorie as k INNER JOIN kontocat as kc on kc.kategorie_ID = k.kategorie_ID WHERE KC.Recnum_ID = 4 OR k.Standartwert = 1");
      if($query->row() == null) return;
      else
      {
